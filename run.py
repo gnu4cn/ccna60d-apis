59bd09d3d7ade40780df0569640b24657e9c6b0f
gnu4cn <unisko@gmail.com>
2019-11-22 21:25:55 +0800

    uwsgi added, ready for nginx

---
+++ run.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+from flask import Flask
+from sqlalchemy_utils import database_exists
+
+from api.conf.config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFACATIONS
+from api.conf.routes import generate_routes
+from api.database.database import db
+from api.schemas.schemas import ma
+from api.db_initializer.db_initializer import (create_admin_user,
+                                               create_super_admin,
+                                               create_test_user)
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
+
+logging.basicConfig(filename='my.log', level=logging.DEBUG, format=LOG_FORMAT,
+                    datefmt=DATE_FORMAT)
+
+# Create a flask app.
+app = Flask(__name__)
+
+# Set debug true for catching the errors.
+app.config['DEBUG'] = True
+
+# Set database url.
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFACATIONS
+
+# Database initialize with app.
+db.init_app(app)
+
+# Marshmallow initialize with app.
+# 避免循环导入
+ma.init_app(app)
+
+
+# New db app if no database.
+db.app = app
+
+db.create_all()
+
+generate_routes(app)
+
+if __name__ == '__main__':
+
+    # Run app.
+    app.run()
